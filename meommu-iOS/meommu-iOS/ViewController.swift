3292e00fbd027e8a62d184c212b7bbd8c48d21b6
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-09-17 22:43:03 +0900

    add: 프로젝트 생성

---
+++ meommu-iOS/meommu-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  meommu-iOS
+//
+//  Created by 이예빈 on 2023/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

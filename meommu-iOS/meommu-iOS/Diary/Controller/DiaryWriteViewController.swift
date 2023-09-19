2ff84ad2f29a350148ce9aca80ca35dacbd0c25b
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-09-19 22:45:49 +0900

    feat: 일기쓰기 화면 구성

---
+++ meommu-iOS/meommu-iOS/Diary/Controller/DiaryWriteViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryWriteViewController.swift
+//  meommu-iOS
+//
+//  Created by 이예빈 on 2023/09/19.
+//
+
+import UIKit
+
+class DiaryWriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}

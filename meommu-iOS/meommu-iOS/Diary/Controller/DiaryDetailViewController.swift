9a079ff3f91150578d4f41fdde00878bd3758550
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-09-30 15:05:27 +0900

    feat: 일기 수정 및 삭제 바텀시트 뷰 컨트롤러 구현

--- meommu-iOS/meommu-iOS/Diary/Controller/DiaryDetailViewController.swift
+++ meommu-iOS/meommu-iOS/Diary/Controller/DiaryDetailViewController.swift
@@ -15,12 +15,25 @@ class DiaryDetailViewController: UIViewController {
         updateUI()
     }
     
+    // -----------------------------------------
+    // 일기 수정 및 삭제 바텀시트 생성하기
+    @IBOutlet var diaryReviseButton: UIBarButtonItem!
+    
+    @IBAction func OnClick_diaryRiviseButton(_ sender: Any) {
+        let vc = UIStoryboard(name: "DiaryRevise", bundle: nil).instantiateViewController(identifier: "DiaryReviseViewController") as! DiaryReviseViewController
+        
+        presentPanModal(vc)
+    }
+    
+    
+    // -----------------------------------------
     // 뒤로 가기 버튼
     @IBOutlet var backButton: UIBarButtonItem!
     @IBAction func OnClick_BackButton(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
     
+    // -----------------------------------------
     // 전달 받은 데이터
     @IBOutlet var diaryDate: UILabel!
     @IBOutlet var diaryDetail: UILabel!

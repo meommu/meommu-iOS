998857561402b9dc340d5e2eb770a2e604b9730e
zaehorang <zaehorang2@naver.com>
2023-10-01 18:45:15 +0900

    fix: Update corner radius for nextButton

--- meommu-iOS/meommu-iOS/Onboarding/Controller/OnboardingViewController.swift
+++ meommu-iOS/meommu-iOS/Onboarding/Controller/OnboardingViewController.swift
@@ -9,21 +9,22 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setCornerRadius()
     }
     
+    //MARK: - 코너 레디어스 값 설정 메서드
+    
+    func setCornerRadius() {
+        
+        nextButton.setCornerRadius(6.0)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }

2906ade59ac1a8acb0d0dba6a8e21209205ad47c
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-10-15 20:53:47 +0900

    feat: 일기 디테일 뷰 화면
    페이지 컨트롤러를 활용해 사진 여러 장 조회 기능 구현

--- meommu-iOS/meommu-iOS/Diary/Controller/DiaryDetailViewController.swift
+++ meommu-iOS/meommu-iOS/Diary/Controller/DiaryDetailViewController.swift
@@ -22,6 +22,14 @@ class DiaryDetailViewController: UIViewController {
         let diaryReviseVC = storyboard.instantiateViewController(withIdentifier: "DiaryReviseViewController") as! DiaryReviseViewController
         
         fpc.set(contentViewController: diaryReviseVC)
+        
+        // 페이지 컨트롤 설정
+        if let selectedDiary = diary {
+                    imageArray = Array(selectedDiary.diaryImage) // 이미지 배열 초기화
+                    diaryimagepageControl.numberOfPages = imageArray.count // 페이지 컨트롤 설정
+                    diaryimagepageControl.currentPage = 0
+                }
+        
     }
     
     // -----------------------------------------
@@ -33,7 +41,6 @@ class DiaryDetailViewController: UIViewController {
     @IBAction func OnClick_diaryRiviseButton(_ sender: Any) {
         present(fpc, animated: true, completion: nil)
     }
-    
     
     // -----------------------------------------
     // 뒤로 가기 버튼
@@ -50,20 +57,37 @@ class DiaryDetailViewController: UIViewController {
     @IBOutlet var diaryName: UILabel!
     @IBOutlet var diaryImageView: UIImageView!
     
-    var Image: String?
-    var Title: String?
-    var Detail: String?
-    var Date: String?
-    var Name: String?
+    var diary : Diary?
+    
     
     func updateUI(){
-        if let Image = self.Image, let Title = self.Title, let Detail = self.Detail, let Date = self.Date {
-            
-            diaryImageView.image = UIImage(named: Image)
-            diaryTitle.text = Title
-            diaryDetail.text = Detail
-            diaryDate.text = Date
-            diaryName.text = Name
+        guard let selectedDiary = diary else { return }
+        
+        diaryDate.text = selectedDiary.diaryDate
+        diaryDetail.text = selectedDiary.diaryDetail
+        diaryTitle.text = selectedDiary.diaryTitle
+        diaryName.text = selectedDiary.diaryName
+        
+        if let imageName = selectedDiary.diaryImage.first,
+           let image = UIImage(named: imageName) {
+            diaryImageView.image = image
+        }
+    }
+    
+    // -----------------------------------------
+    // 이미지 페이지 컨트롤
+    
+    var imageArray: [String] = [] // 이미지 저장 배열
+    
+    @IBOutlet var diaryimagepageControl: UIPageControl!
+    
+    @IBAction func diaryimagePageChange(_ sender: UIPageControl) {
+        
+        let currentPageIndex = sender.currentPage < imageArray.count ? sender.currentPage : 0
+        
+        let imageName = imageArray[currentPageIndex]
+        if let image = UIImage(named:imageName) {
+            diaryImageView.image = image
         }
     }
     

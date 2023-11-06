d5bce3988f44b8e6bb5fd06af4d042eef4a7a7d2
Yecong <84004751+leeyebeen-dev@users.noreply.github.com>
2023-11-06 22:23:57 +0900

    feat: 멈무일기 가이드 1단계 바텀시트

--- meommu-iOS/meommu-iOS/Diary/Controller/BottomSheet/StepOneViewController.swift
+++ meommu-iOS/meommu-iOS/Diary/Controller/BottomSheet/StepOneViewController.swift
@@ -6,9 +6,11 @@
 //
 
 import UIKit
+import PanModal
 
 
-class StepOneViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class StepOneViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, PanModalPresentable {
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +25,37 @@ class StepOneViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     // -----------------------------------------
     // 2단계 바텀 시트 생성하기
+
+    var panScrollable: UIScrollView? {
+        return nil
+    }
     
+    // 접혔을 때
+    var shortFormHeight: PanModalHeight {
+        return .contentHeight(522)
+    }
+
+    // 펼쳤을 때
+    var longFormHeight: PanModalHeight {
+        return .maxHeightWithTopInset(290)
+    }
+
+    var cornerRadius: CGFloat {
+        return 20
+    }
+
+    var allowsTapToDismiss: Bool {
+        return true
+    }
+
+    var allowsDragToDismiss: Bool {
+        return true
+    }
+
+    // 최상단 스크롤 불가
+    var anchorModalToLongForm: Bool {
+        return true
+    }
     
     // TableView 생성하기
     

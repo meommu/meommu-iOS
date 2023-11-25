5a3b3243f3df4107f3b71109e8c4fee94119bb93
zaehorang <jaho001@uos.ac.kr>
2023-11-26 03:54:27 +0900

    feat: 비밀번호 찾기 화면 구현

---
+++ meommu-iOS/meommu-iOS/Login/Controller/PasswordRecoveryThirdViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PasswordRecoveryThirdViewController.swift
+//  meommu-iOS
+//
+//  Created by zaehorang on 2023/11/26.
+//
+
+import UIKit
+
+class PasswordRecoveryThirdViewController: UIViewController {
+
+    // 버튼 프로퍼티
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    //MARK: - 이전 화면 버튼
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+
+}

b7f5d7325177df8e60d26159ede4f0b35d334e61
zaehorang <jaho001@uos.ac.kr>
2023-11-27 21:16:16 +0900

    feat(비밀번호 찾기): corner radius 추가

--- meommu-iOS/meommu-iOS/Login/Controller/PasswordRecoveryThirdViewController.swift
+++ meommu-iOS/meommu-iOS/Login/Controller/PasswordRecoveryThirdViewController.swift
@@ -13,13 +13,20 @@ class PasswordRecoveryThirdViewController: UIViewController {
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
     
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupCornerRadius()
     }
     
-
+    //MARK: - 코너 레디어스 값 설정 메서드
+    private func setupCornerRadius() {
+        nextButton.setCornerRadius(6.0)
+        passwordTextField.setCornerRadius(4.0)
+    }
+    
     //MARK: - 이전 화면 버튼
     @IBAction func backButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true)

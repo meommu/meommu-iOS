ba68486844345f7615fa98b169ba418ed1fdac24
zaehorang <jaho001@uos.ac.kr>
2023-12-04 15:57:19 +0900

    fix: 비밀번호 찾기 화면 UI 수정

--- meommu-iOS/meommu-iOS/Login/Controller/PasswordRecoveryFourthViewController.swift
+++ meommu-iOS/meommu-iOS/Login/Controller/PasswordRecoveryFourthViewController.swift
@@ -18,7 +18,28 @@ class PasswordRecoveryFourthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupButton()
+        setupTextField()
         setupCornerRadius()
+        setupDelegate()
+    }
+    
+    //MARK: - 버턴 셋업 메서드
+    private func setupButton() {
+        // 다음 버튼 색상
+        nextButton.backgroundColor = .gray300
+        nextButton.setTitleColor(.white, for: .normal)
+        
+        // 백 버튼 색상
+        backButton.tintColor = .gray400
+        
+        nextButton.isEnabled = false
+    }
+    
+    //MARK: - 텍스트 필드 셋업 메서드
+    func setupTextField() {
+        confirmPasswordTextField.backgroundColor = .gray100
+        confirmPasswordTextField.addLeftPadding()
     }
     
     //MARK: - 코너 레디어스 값 설정 메서드
@@ -27,9 +48,54 @@ class PasswordRecoveryFourthViewController: UIViewController {
     
         confirmPasswordTextField.setCornerRadius(4.0)
     }
-    //MARK: - 이전 화면 버튼
+    
+    //MARK: - 델리게이트 셋업 메서드
+    private func setupDelegate() {
+        // textField 델리게이트 설정
+        confirmPasswordTextField.delegate = self
+    }
+    
+    //MARK: - 이전 화면 탭 버튼
     @IBAction func backButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
 
+    //MARK: - 다음 버튼 탭 메서드
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        
+        // 1. 이전 화면에서 입력한 비밀번호와 같은 것이 입렫되었는지 확인
+        // 2. 비밀번호를 바꾸는 api 메서드 작성
+        // 3. 로그인 화면으로 이동
+        changeRootViewToLogin()
+        
+        // 4. 토스트 얼럿 띄우기
+    }
+    
+    // 루트 뷰를 로그인 화면으로 변경하는 메서드
+    private func changeRootViewToLogin() {
+        // 로그인하고 일기 화면으로 rootView 변경
+        let newStoryboard = UIStoryboard(name: "Login", bundle: nil)
+        let newViewController = newStoryboard.instantiateViewController(identifier: "LoginFirstViewController")
+        
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        
+        sceneDelegate.changeRootViewController(newViewController, animated: true)
+    }
+    
+}
+
+//MARK: - UITextFieldDelegate 확장
+extension PasswordRecoveryFourthViewController: UITextFieldDelegate {
+    //MARK: - 다음 버튼 활성화 메서드
+    private func updateLoginButtonState() {
+        // 다음 버튼 활성화 조건 확인
+        guard let password = confirmPasswordTextField.text, !password.isEmpty, password.isPasswordFormatValid()
+        else {
+            nextButton.backgroundColor = .gray300
+            nextButton.isEnabled = false
+            return
+        }
+        nextButton.backgroundColor = .prilmaryA
+        nextButton.isEnabled = true
+    }
 }
--- meommu-iOS/meommu-iOS/Login/Controller/PasswordRecoveryThirdViewController.swift
+++ meommu-iOS/meommu-iOS/Login/Controller/PasswordRecoveryThirdViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class PasswordRecoveryThirdViewController: UIViewController {
-
+    
     // 버튼 프로퍼티
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
@@ -17,8 +17,29 @@ class PasswordRecoveryThirdViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupButton()
+        setupTextField()
         setupCornerRadius()
+        setupDelegate()
+    }
+    
+    //MARK: - 버턴 셋업 메서드
+    private func setupButton() {
+        // 다음 버튼 색상
+        nextButton.backgroundColor = .gray300
+        nextButton.setTitleColor(.white, for: .normal)
+        
+        // 백 버튼 색상
+        backButton.tintColor = .gray400
+        
+        nextButton.isEnabled = false
+    }
+    
+    //MARK: - 텍스트 필드 셋업 메서드
+    func setupTextField() {
+        passwordTextField.backgroundColor = .gray100
+        passwordTextField.addLeftPadding()
     }
     
     //MARK: - 코너 레디어스 값 설정 메서드
@@ -27,9 +48,31 @@ class PasswordRecoveryThirdViewController: UIViewController {
         passwordTextField.setCornerRadius(4.0)
     }
     
+    //MARK: - 델리게이트 셋업 메서드
+    private func setupDelegate() {
+        // textField 델리게이트 설정
+        passwordTextField.delegate = self
+    }
+    
     //MARK: - 이전 화면 버튼
     @IBAction func backButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
+    
+}
 
+//MARK: - UITextFieldDelegate 확장
+extension PasswordRecoveryThirdViewController: UITextFieldDelegate {
+    //MARK: - 다음 버튼 활성화 메서드
+    private func updateLoginButtonState() {
+        // 다음 버튼 활성화 조건 확인
+        guard let password = passwordTextField.text, !password.isEmpty, password.isPasswordFormatValid()
+        else {
+            nextButton.backgroundColor = .gray300
+            nextButton.isEnabled = false
+            return
+        }
+        nextButton.backgroundColor = .prilmaryA
+        nextButton.isEnabled = true
+    }
 }

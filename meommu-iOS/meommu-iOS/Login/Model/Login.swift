c332b3134878a80cca0ff4a94343d8ad5ada3bd3
zaehorang <jaho001@uos.ac.kr>
2023-12-27 02:47:35 +0900

    feat: 액세스 토큰으로 자동 로그인 기능 구현

--- meommu-iOS/meommu-iOS/Login/Model/Login.swift
+++ meommu-iOS/meommu-iOS/Login/Model/Login.swift
@@ -27,8 +27,6 @@ struct TokenData: Codable {
 
 // MARK: - 로그인 정보 조회 응답 -> 토큰으로 로그인
 
-
-
 struct TokenVerificationResponse: Codable {
     let code, message: String
     let loginData: LoginData?
@@ -43,7 +41,7 @@ struct TokenVerificationResponse: Codable {
 
 // 로그인 정보 조회 응답 데이터
 struct LoginData: Codable {
-    let id: Int?
-    let name, email: String?
+    let id: Int
+    let name, email, createdAt: String
 }
 
